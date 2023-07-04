8b9ca4e5837d0f59b2a61f5a85ac97e2d0678013
arjunn881 <arjunn881@gmail.com>
2023-07-04 15:58:56 +0530

    04/07 15:58

--- src/intro/intro.jsx
+++ src/intro/intro.jsx
@@ -12,7 +12,7 @@ export default function Intro() {
       showCursor: true,
       backDelay: 1500,
       backSpeed: 60,
-      strings: ["Front-end"],
+      strings: ["MERN-Stack"],
     });
   }, []);
   
