19ebdce239d7e7ec6f21e71748801dcdcccc988a
arjunn881 <arjunn881@gmail.com>
2023-09-21 00:12:56 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,23 @@ import { useState } from "react";
 import Menu from "./menu/menu";
 
 function App() {
-  const [menuOpen,setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-     <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-     <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-     <div className="sections">
-       <Intro/>
-       <Portfolio/>
-       
-       <Contact/>
-     </div>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Contact />
+      </div>
     </div>
   );
-  
 }
 
+
 export default App;
+
+
+
+
